51bb6b0b98095815210faea53bfc51fcd7a091ee
rumanaahashmi <rumanahashmi451@gmail.com>
2026-06-24 18:12:05 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,15 @@
+name = 'talat hashmi'
+print(name)
+phone_number = 9121783389
+print(phone_number)
+college_name = 'ms junior college'
+print(college_name)
+
+a = 9.87
+b = 8
+f = 90
+c = a + b + f
+print(c) 
+
+
+
---
+++ hello.py
@@ -0,0 +1,44 @@
+a = "40"
+b = 5
+c = 10
+d = 15
+e = "2"
+f = "3"
+print (int(a)+ int(e) + int(f))  
+print (b - c + d)
+print (int(a) + int(b) + int(f)) #convert string to int
+maths = 79
+print (f"my marks in maths is = {maths}") 
+
+a = 6 
+b = ""
+c = False
+
+print (type(a))
+print (type(b))
+print (type(c))
+
+
+a = 6 
+b = 5 
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+x = 10
+y = 20
+x,y=y,x
+print(x)
+print(y)
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+print(a + b + c)
+print(a * b * c)
+print(a/b/c)
+print(a**b)
+
